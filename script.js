e744fe7edaf929d1b50e32874ab32f16a7cbb26a
dornokh <david@dornokh.com>
2026-06-05 15:33:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const backgrounds = document.querySelectorAll('.background');
+
+let currentX = 0;
+let currentY = 0;
+
+document.addEventListener('mousemove',(e)=>{
+
+const x = (e.clientX / window.innerWidth - 0.5) * 16;
+const y = (e.clientY / window.innerHeight - 0.5) * 16;
+
+currentX = x;
+currentY = y;
+
+backgrounds.forEach(bg=>{
+bg.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+});
+
+if (window.DeviceOrientationEvent) {
+
+window.addEventListener('deviceorientation',(event)=>{
+
+const x = (event.gamma || 0) * 0.4;
+const y = (event.beta || 0) * 0.2;
+
+backgrounds.forEach(bg=>{
+bg.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+});
+
+}
