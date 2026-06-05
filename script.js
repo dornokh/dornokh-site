5c839b5011eca30c93a8dc0ca1b391d5ba57bdf2
dornokh <david@dornokh.com>
2026-06-05 16:11:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,62 +6,30 @@ let targetY = 0;
 let currentX = 0;
 let currentY = 0;
 
+document.addEventListener('mousemove', (e) => {
+    targetX = (e.clientX / window.innerWidth - 0.5) * 35;
+    targetY = (e.clientY / window.innerHeight - 0.5) * 35;
+});
+
+document.addEventListener('touchmove', (e) => {
+    const touch = e.touches[0];
+
+    targetX = (touch.clientX / window.innerWidth - 0.5) * 25;
+    targetY = (touch.clientY / window.innerHeight - 0.5) * 25;
+
+}, { passive: true });
+
 function animate() {
 
-    currentX += (targetX - currentX) * 0.04;
-    currentY += (targetY - currentY) * 0.04;
+    currentX += (targetX - currentX) * 0.03;
+    currentY += (targetY - currentY) * 0.03;
 
     backgrounds.forEach(bg => {
-
         bg.style.transform =
-            `translate3d(${currentX}px, ${currentY}px, 0) scale(1.06)`;
-
+            `translate3d(${currentX}px, ${currentY}px, 0) scale(1.08)`;
     });
 
     requestAnimationFrame(animate);
 }
 
 animate();
-
-document.addEventListener('mousemove', (e) => {
-
-    targetX =
-        (e.clientX / window.innerWidth - 0.5) * 30;
-
-    targetY =
-        (e.clientY / window.innerHeight - 0.5) * 30;
-
-});
-
-document.addEventListener('touchmove', (e) => {
-
-    const touch = e.touches[0];
-
-    targetX =
-        (touch.clientX / window.innerWidth - 0.5) * 24;
-
-    targetY =
-        (touch.clientY / window.innerHeight - 0.5) * 24;
-
-}, { passive: true });
-
-if (window.DeviceOrientationEvent) {
-
-    window.addEventListener('deviceorientation', (event) => {
-
-        const gamma = event.gamma || 0;
-        const beta = event.beta || 0;
-
-        targetX = gamma * 1.2;
-        targetY = beta * 0.8;
-
-    });
-
-}        const beta = event.beta || 0;
-
-        targetX = gamma * 0.8;
-        targetY = beta * 0.4;
-
-    });
-
-}
